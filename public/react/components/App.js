2a63a86cc0bc2a3e2ae741d7d485a0c7ea2b6da9
4mb3r05 <a.yardley2005@gmail.com>
2023-11-23 15:51:10 +0000

    Revert "Item page components 1"
    This reverts commit 299d38e2a7e27e16800787ddd677025815e86541.

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { SaucesList } from './SaucesList';
-import ItemPage from './ItemPage';
-import './react/style.css';
-import NavigationBar from '.ItemPage/NavigationBar';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
@@ -33,28 +30,4 @@ export const App = () => {
 			<SaucesList sauces={sauces} />
 		</main>
 	)
-};
-
-
-const App = () => {
-  // Replace these with actual data from your application state or API
-  const product = { id: 1, name: 'Sample Product', description: 'Sample description', price: 50.00 };
-  const relatedProducts = [{ id: 2, name: 'Related Product 1' }, { id: 3, name: 'Related Product 2' }];
-  const reviews = [{ id: 1, comment: 'Great product!', rating: 5 }, { id: 2, comment: 'Not bad', rating: 3 }];
-
-  return (
-    <div>
-      <ItemPage product={product} relatedProducts={relatedProducts} reviews={reviews} />
-    </div>
-  );
-  
-  return (
-    <div>
-      <Header />
-      <NavigationBar />
-      <ItemPage product={product} relatedProducts={relatedProducts} reviews={reviews} />
-    </div>
-  );
-};
-
-export default App;
+}
